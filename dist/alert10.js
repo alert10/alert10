9df9ff894e4e353878bd5e51b7f5fb466624d7ad
Fin :3 <118539051+GreenestGoat@users.noreply.github.com>
2025-03-12 11:24:24 +0000

    Update alert10.js

--- dist/alert10.js
+++ dist/alert10.js
@@ -1,1 +1,18 @@
+((global) => {
+  let alert10 = async ({ title = "Notification", ...opts } = {}) => {
+    if (!("Notification" in window)) return;
 
+    let show = () => new Notification(title, opts);
+
+    let permissionStatus = await Notification.requestPermission();
+    if (permissionStatus === "granted") show();
+  };
+
+  Object.assign(alert10, {
+    requestPermission: () => Notification?.requestPermission?.(),
+    getPermissionStatus: () => Notification?.permission ?? "not-supported",
+    isSupported: "Notification" in window,
+  });
+
+  global.alert10 = alert10;
+})(globalThis);
